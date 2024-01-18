d55ab158247ba22910439d16b15eee3621aca998
Karan Sanghvi <karansanghvi1303@gmail.com>
2024-01-18 12:43:27 +0530

    demo

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+  import React from 'react';
+  import { Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  export default function App() {
+    return (
+    <View className="flex-1 items-center justify-center bg-white">
+        <Text className="text-gray-400 text-md font-bold mt-2 align-middle">
+            Open up App.js to start working on your app!
+        </Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
