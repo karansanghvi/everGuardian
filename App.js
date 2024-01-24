d23ac2946ef15e196d90d1aa1d53fe236936f4dc
Karan Sanghvi <karansanghvi1303@gmail.com>
2024-01-24 17:10:20 +0530

    added poppins font

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Text, View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import { useFonts } from 'expo-font';
 
 export default function App() {
+
+  let [fontsLoaded] = useFonts({
+    'Poppins': require('./assets/fonts/Poppins/Poppins-Black.ttf') 
+  });
+  
+  if(!fontsLoaded) {
+    return <AppLoading/>
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>hello world</Text>
-      <StatusBar style="auto" />
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text style={
+        {
+          fontFamily: 'Poppins-Black', fontSize: 40
+        }
+      }>
+        Hello World
+      </Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
