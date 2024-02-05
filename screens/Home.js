07760a4186de2a73d953545cfdfffbfe05be20b6
Vivaash Jain <vivaashjain2@gmail.com>
2024-02-05 12:27:54 +0530

    Change Made in Home Screen

--- screens/Home.js
+++ screens/Home.js
@@ -4,7 +4,7 @@ import { View, Text, StyleSheet } from 'react-native';
 const Home = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home Screen</Text>
+      <Text style={styles.text}>This the Home Page</Text>
     </View>
   );
 }
