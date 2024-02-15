98bc3de047920b84df5b83b86ace9ded9c87d701
Karan Sanghvi <karansanghvi1303@gmail.com>
2024-02-15 12:42:36 +0530

    added blood group button

--- screens/MedicalRecords.js
+++ screens/MedicalRecords.js
@@ -1,11 +1,15 @@
 import { View, Text, TouchableOpacity, TextInput, Image, ScrollView, StyleSheet } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { LinearGradient } from 'expo-linear-gradient'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { useNavigation } from '@react-navigation/native'
 import {ArrowLeftIcon} from 'react-native-heroicons/solid'
+import { Dropdown } from 'react-native-element-dropdown'
+import AntDesign from '@expo/vector-icons/AntDesign'
 
 const MedicalRecords = () => {
+
+  const [bloodGroup, setBloodGroup] = useState(null);
 
   const navigation = useNavigation();
   const handleLogin = () => {
@@ -14,6 +18,17 @@ const MedicalRecords = () => {
   const handleSignupButton = () => {
     navigation.navigate('Home');
   }
+
+  const bloodGroupData = [
+    { label: 'A+', value: '1' },
+    { label: 'O+', value: '2' },
+    { label: 'B+', value: '3' },
+    { label: 'AB+', value: '4' },
+    { label: 'A-', value: '5' },
+    { label: 'O-', value: '6' },
+    { label: 'B-', value: '7' },
+    { label: 'AB-', value: '8' },
+  ];
 
   return (
     <LinearGradient
@@ -36,11 +51,27 @@ const MedicalRecords = () => {
       <ScrollView style={{ flex: 1 }}>
         <View className="rounded-tl-2xl rounded-tr-2xl flex-1 bg-white px-8 pt-8 mt-6">
           <View className="form space-y-2">
-            <Text className="text-black ml-1 text-lg">Enter Blood Group:</Text>
-              <TextInput
-                className="p-4 bg-gray-100 text-black rounded-xl mb-3"
-                placeholder='Blood Group'
-              />
+            <Dropdown
+              style={styles.dropdown}
+              placeholderStyle={styles.placeholderStyle}
+              selectedTextStyle={styles.selectedTextStyle}
+              inputSearchStyle={styles.inputSearchStyle}
+              iconStyle={styles.iconStyle}
+              data={bloodGroupData}
+              search
+              maxHeight={300}
+              labelField="label"
+              valueField="value"
+              placeholder="Select Blood Group"
+              searchPlaceholder='Search...'
+              value={bloodGroup}
+              onChange={item => {
+                setBloodGroup(item.value);
+              }}
+              renderLeftIcon={() => (
+                <AntDesign style={styles.icon} color="black" name="Safety" size={20} />
+              )}
+            />
             <Text className="text-black ml-1 text-lg">Enter Last Name:</Text>
               <TextInput
                 className="p-4 bg-gray-100 text-black rounded-xl mb-3"
@@ -123,6 +154,29 @@ const styles = StyleSheet.create({
   },
   orText: {
     color: 'black',
+    fontSize: 16,
+  },
+  dropdown: {
+    margin: 16,
+    height: 50,
+    borderBottomColor: 'gray',
+    borderBottomWidth: 0.5,
+  },
+  icon: {
+    marginRight: 5,
+  },
+  placeholderStyle: {
+    fontSize: 16,
+  },
+  selectedTextStyle: {
+    fontSize: 16,
+  },
+  iconStyle: {
+    width: 20,
+    height: 20,
+  },
+  inputSearchStyle: {
+    height: 40,
     fontSize: 16,
   },
 });
