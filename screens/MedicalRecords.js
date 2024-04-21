7786f883b4ae14d0430e3bd4c90cf5c821666d30
Karan Sanghvi <karansanghvi1303@gmail.com>
2024-04-21 12:44:19 +0530

    no changes

--- screens/MedicalRecords.js
+++ screens/MedicalRecords.js
@@ -1,7 +1,7 @@
 import { 
   View, Text, TouchableOpacity, TextInput, Image, ScrollView, StyleSheet, Platform, Button
  } from 'react-native'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { useNavigation } from '@react-navigation/native'
 import {ArrowLeftIcon} from 'react-native-heroicons/solid'
@@ -12,6 +12,11 @@ const MedicalRecords = () => {
 
   const [bloodGroup, setBloodGroup] = useState(null);
   const [gender, setGender] = useState(null);
+  const [covid, setCovid] = useState(null);
+  const [allergy, setAllergy] = useState('');
+  const [medications, setMedications] = useState('');
+  const [diseases, setDiseases] = useState('');
+
   const navigation = useNavigation();
 
   const handleSubmitButton = () => {
@@ -33,7 +38,12 @@ const MedicalRecords = () => {
     { label: 'Male', value: '1' },
     { label: 'Female', value: '2' },
     { label: 'Other', value: '3' },
-  ]
+  ];
+
+  const covidData = [
+    { label: 'Yes', value: '1' },
+    { label: 'No', value: '2' },
+  ];
 
   return (
     <>
@@ -93,11 +103,27 @@ const MedicalRecords = () => {
                   <AntDesign style={styles.icon} color="black" name="Safety" size={20} />
                 )}
             />          
-            <Text className="text-black ml-1 text-lg">Enter Address:</Text>
-              <TextInput
-                className="p-4 bg-gray-100 text-black rounded-xl mb-3"
-                placeholder='Address'
-              />
+            <Dropdown
+                style={styles.dropdown}
+                placeholderStyle={styles.placeholderStyle}
+                selectedTextStyle={styles.selectedTextStyle}
+                inputSearchStyle={styles.inputSearchStyle}
+                iconStyle={styles.iconStyle}
+                data={covidData}
+                search
+                maxHeight={300}
+                labelField="label"
+                valueField="value"
+                placeholder="COVID Vaccine Taken?"
+                searchPlaceholder='Search...'
+                value={covid}
+                onChange={item => {
+                  setCovid(item.value);
+                }}
+                renderLeftIcon={() => (
+                  <AntDesign style={styles.icon} color="black" name="Safety" size={20} />
+                )}
+            />          
             <Text className="text-black ml-1 text-lg">Any Alergy?</Text>
               <TextInput
                 className="p-4 bg-gray-100 text-black rounded-xl mb-3"
@@ -112,6 +138,31 @@ const MedicalRecords = () => {
               <TextInput
                 className="p-4 bg-gray-100 text-black rounded-xl mb-3"
                 placeholder='Current Diseases'
+              />
+            <Text className="text-black ml-1 text-lg">Any Surgery Done?</Text>
+              <TextInput
+                className="p-4 bg-gray-100 text-black rounded-xl mb-3"
+                placeholder='Surgery'
+              />
+            <Text className="text-black ml-1 text-lg">Insurance Information:</Text>
+              <TextInput
+                className="p-4 bg-gray-100 text-black rounded-xl mb-3"
+                placeholder='Insurance Provider'
+              />
+              <TextInput
+                className="p-4 bg-gray-100 text-black rounded-xl mb-3"
+                placeholder='Policy Number'
+                keyboardType='numeric'
+              />
+            <Text className="text-black ml-1 text-lg">Emergency Contact Details:</Text>
+              <TextInput
+                className="p-4 bg-gray-100 text-black rounded-xl mb-3"
+                placeholder='Full Name'
+              />
+              <TextInput
+                className="p-4 bg-gray-100 text-black rounded-xl mb-3"
+                placeholder='Phone Number'
+                keyboardType='numeric'
               />
               <TouchableOpacity 
                 className="py-3 bg-black rounded-lg mb-4"
